a9dc22890df5e7a0d831ea8822c60bdd65803743
Cody Olsen <stipsan@gmail.com>
2024-11-21 04:17:34 +0100

    Update actions.ts

--- next-canary/src/app/actions.ts
+++ next-canary/src/app/actions.ts
@@ -12,6 +12,8 @@ export async function randomColorTheme(tags: SyncTag[]) {
   const res = await fetch('https://lcapi-examples-api.sanity.dev/api/random-color-theme', {
     method: 'PUT',
   })
+  // Wait 1 second to stagger requests a little bit
+  await new Promise((resolve) => setTimeout(resolve, 1_000))
   expireTag(...tags)
   return res.json()
 }
