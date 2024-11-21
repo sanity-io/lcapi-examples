3a579cc3e3d2533db29a1528b36683fd681086b0
Cody Olsen <stipsan@gmail.com>
2024-11-21 16:35:13 +0100

    Update actions.ts

--- next-enterprise/src/app/actions.ts
+++ next-enterprise/src/app/actions.ts
@@ -1,12 +1,14 @@
 'use server'
 
 import type {SyncTag} from '@sanity/client'
-import {expireTag} from 'next/cache'
+import {revalidateTag} from 'next/cache'
 
 export async function randomColorTheme(tags: SyncTag[]) {
   const res = await fetch('https://lcapi-examples-api.sanity.dev/api/random-color-theme', {
     method: 'PUT',
   })
-  expireTag(...tags)
+  for (const tag of tags) {
+    revalidateTag(tag)
+  }
   return res.json()
 }
