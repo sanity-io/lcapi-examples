e24a59b41903178f14c9119a59276e165e14709a
Cody Olsen <stipsan@gmail.com>
2024-11-21 14:24:25 +0100

    Update next.config.ts

--- next-canary/next.config.ts
+++ next-canary/next.config.ts
@@ -3,6 +3,13 @@ import type {NextConfig} from 'next'
 const nextConfig: NextConfig = {
   experimental: {
     dynamicIO: true,
+    cacheLife: {
+      default: {
+        stale: undefined,
+        revalidate: 60 * 60 * 24 * 30, // 1 month, the default is 15 minutes
+        expire: 0xfffffffe,
+      },
+    },
   },
 }
 
