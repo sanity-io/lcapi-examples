5c0b217200f86f23f5fd44b9780f818c20aafa59
Cody Olsen <stipsan@gmail.com>
2024-12-12 13:43:15 +0100

    liftoff

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -2,7 +2,10 @@
 import vercel from '@astrojs/vercel'
 import {defineConfig} from 'astro/config'
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
   adapter: vercel(),
+  integrations: [tailwind()],
 })
