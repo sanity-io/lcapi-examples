d27c92facd0d5ca8cd7c971f7b0b493a837af6ee
Cody Olsen <stipsan@gmail.com>
2025-02-13 11:27:16 +0100

    add theme button support, and more

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -4,10 +4,16 @@ import {defineConfig} from 'astro/config'
 import tailwindcss from "@tailwindcss/vite";
 
 
+import react from '@astrojs/react';
+
+
 // https://astro.build/config
 export default defineConfig({
   adapter: vercel({isr: true, skewProtection: true}),
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [react()],
 })
