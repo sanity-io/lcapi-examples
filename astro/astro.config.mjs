93e87bb36de0bcfc9b5b74039b6daf3cdd8c54b8
Cody Olsen <81981+stipsan@users.noreply.github.com>
2025-02-13 11:37:38 +0100

    Improve astro (#79)
    * move astro to tailwind v4
    
    * handle live for both queries
    
    * add theme button support, and more
    
    * Update pnpm-lock.yaml
    
    * remove ISR

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,10 +1,19 @@
 // @ts-check
-import tailwind from '@astrojs/tailwind'
 import vercel from '@astrojs/vercel'
 import {defineConfig} from 'astro/config'
+import tailwindcss from "@tailwindcss/vite";
+
+
+import react from '@astrojs/react';
+
 
 // https://astro.build/config
 export default defineConfig({
   adapter: vercel(),
-  integrations: [tailwind()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [react()],
 })
--- astro/src/components/ThemeButton.tsx
+++ astro/src/components/ThemeButton.tsx
@@ -1,14 +1,15 @@
-'use client'
-
 import {useTransition} from 'react'
-import {randomColorTheme} from './actions'
 
-export function ThemeButton() {
+export default function ThemeButton() {
   const [pending, startTransition] = useTransition()
   return (
     <button
       disabled={pending}
-      onClick={() => startTransition(() => randomColorTheme())}
+      onClick={() => startTransition(async () => {
+        await fetch('https://lcapi-examples-api.sanity.dev/api/random-color-theme', {
+          method: 'PUT',
+        })
+      })}
       className={`cursor-pointer bg-theme-button text-theme-button focus:ring-theme focus:ring-offset-theme rounded-md px-4 py-2 text-sm font-semibold transition ease-in-out focus:outline-hidden focus:ring-2 focus:ring-opacity-50 focus:ring-offset-2 focus:duration-0 disabled:cursor-not-allowed disabled:opacity-50 ${pending ? 'animate-pulse cursor-wait duration-150' : 'duration-1000'} `}
     >
       {pending ? 'Generating...' : 'Random Color Theme'}
