9cb79bb19b3cc8827d269450f9695481cd197fce
Cody Olsen <stipsan@gmail.com>
2025-02-19 00:32:42 +0100

    use current api version

--- next-enterprise/src/sanity/client.ts
+++ next-enterprise/src/sanity/client.ts
@@ -3,6 +3,6 @@ import {createClient} from '@sanity/client'
 export const client = createClient({
   projectId: 'hiomol4a',
   dataset: 'lcapi',
-  apiVersion: '2025-02-19',
+  apiVersion: '2025-02-18',
   useCdn: true,
 })
