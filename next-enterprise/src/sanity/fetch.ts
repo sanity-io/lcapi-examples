001dcadedddabb3111baba6b707d36323aa82f2b
Cody Olsen <stipsan@gmail.com>
2025-02-19 00:20:10 +0100

    set cache life

--- next-enterprise/src/sanity/fetch.ts
+++ next-enterprise/src/sanity/fetch.ts
@@ -1,5 +1,5 @@
 import {type ClientReturn, type QueryParams} from '@sanity/client'
-import {unstable_cacheTag as cacheTag} from 'next/cache'
+import {unstable_cacheLife as cacheLife, unstable_cacheTag as cacheTag} from 'next/cache'
 import {client} from './client'
 
 export async function sanityFetch<const QueryString extends string>({
@@ -12,6 +12,16 @@ export async function sanityFetch<const QueryString extends string>({
   tags?: string[]
 }): Promise<{data: ClientReturn<QueryString, unknown>; tags?: string[]}> {
   'use cache'
+
+  /**
+   * The default cache profile isn't ideal for live content, as it has unnecessary time based background validation, as well as a too lazy client stale value
+   * https://github.com/vercel/next.js/blob/8dd358002baf4244c0b2e38b5bda496daf60dacb/packages/next/cache.d.ts#L14-L26
+   */
+  cacheLife({
+    stale: Infinity,
+    revalidate: Infinity,
+    expire: Infinity,
+  })
 
   const {result, syncTags} = await client.fetch(query, params, {
     filterResponse: false,
