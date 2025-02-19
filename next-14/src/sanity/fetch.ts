4253e92c7b63f5ed0a1133551d46be564e90884b
Cody Olsen <stipsan@gmail.com>
2025-02-19 22:31:37 +0100

    less aggressive expiry

--- next-14/src/sanity/fetch.ts
+++ next-14/src/sanity/fetch.ts
@@ -7,28 +7,32 @@ import {client} from './client'
  * The `defineLive` utility in `next-sanity` handles more advanced use cases, such as live preview, integrating with `sanity/presentation`, and more.
  * @example
  * import {createClient, defineLive} from 'next-sanity'
- * export const {sanityFetch, SanityLive} = defineLive({client: createClient({projectId, dataset, ...})})
+ * export const {sanityFetch, SanityLive} = defineLive({
+ *   client: createClient({projectId, dataset, ...}),
+ *   fetchOptions: {
+ *     revalidate: 15 * 60
+ *   }
+ * })
  */
 export async function sanityFetch<const QueryString extends string>({
   query,
   params = {},
-  tags = [],
 }: {
   query: QueryString
   params?: QueryParams
-  tags?: string[]
-}): Promise<{data: ClientReturn<QueryString, unknown>; tags: string[]}> {
+}): Promise<{data: ClientReturn<QueryString, unknown>; tags?: string[]}> {
   // We have to fetch the sync tags first (this double-fetching is required until the new `cacheTag` API, related to 'use cache', is available in a stable next.js release)
   const {syncTags} = await client.fetch(query, params, {
     filterResponse: false,
     cacheMode: 'noStale',
     tag: 'fetch-sync-tags', // The request tag makes the fetch unique, avoids deduping with the cached query that has tags
-    next: {revalidate: false, tags: ['sanity:fetch-sync-tags', ...tags]},
+    cache: 'force-cache',
+    next: {revalidate: 15 * 60},
   })
-  const cacheTags = [...(syncTags || []), ...tags]
   const data = await client.fetch(query, params, {
     cacheMode: 'noStale',
-    next: {revalidate: false, tags: cacheTags},
+    cache: 'force-cache',
+    next: {revalidate: 15 * 60, tags: syncTags},
   })
-  return {data, tags: cacheTags}
+  return {data, tags: syncTags}
 }
