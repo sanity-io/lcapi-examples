c7d029fb3941566064753988bfc74cb993c90a8b
Cody Olsen <stipsan@gmail.com>
2025-10-22 17:24:15 +0200

    setup nitro

---
+++ tanstack-start/src/components/SanityLive.tsx
@@ -0,0 +1,84 @@
+import {CorsOriginError} from '@sanity/client'
+import {useNavigate, useRouter, useRouterState, useSearch} from '@tanstack/react-router'
+import {useEffect, useEffectEvent} from 'react'
+import {client} from '../utils/sanity'
+import type {LiveEventMessage, LiveEventRestart, LiveEventWelcome} from '@sanity/client'
+
+export function SanityLive() {
+  const selected = useRouterState({select: (state) => state.matches})
+  const lastLiveEventId = useSearch({from: '__root__', select: (state) => state.lastLiveEventId})
+  const navigate = useNavigate({from: '/'})
+  const allTags = selected.flatMap((match) => {
+    if (
+      typeof match.loaderData === 'object' &&
+      match.loaderData !== null &&
+      'tags' in match.loaderData
+    ) {
+      return match.loaderData.tags
+    }
+    return []
+  })
+
+  const router = useRouter()
+  const handleLiveEvent = useEffectEvent(
+    (event: LiveEventMessage | LiveEventRestart | LiveEventWelcome) => {
+      if (event.type === 'welcome') {
+        console.info('Sanity is live with automatic invalidation of published content')
+        navigate({
+          search: (prev) => ({
+            ...prev,
+            lastLiveEventId:
+              // @ts-expect-error - @TODO upgrade `@sanity/client` with the id of welcome events
+              event.id,
+          }),
+          replace: true,
+          resetScroll: false,
+        })
+      } else if (event.type === 'message') {
+        if (event.tags.some((tag) => allTags.includes(tag))) {
+          navigate({
+            search: (prev) => ({...prev, lastLiveEventId: event.id}),
+            replace: true,
+            resetScroll: false,
+          })
+        } else {
+          console.log('no match', event.tags, {allTags})
+        }
+      } else if (event.type === 'restart') {
+        if (lastLiveEventId) {
+          navigate({
+            search: (prev) => ({...prev, lastLiveEventId: undefined}),
+            replace: true,
+            resetScroll: false,
+          })
+        } else {
+          router.invalidate()
+        }
+      }
+    },
+  )
+  useEffect(() => {
+    const subscription = client.live.events().subscribe({
+      next: (event) => {
+        if (event.type === 'message' || event.type === 'restart' || event.type === 'welcome') {
+          handleLiveEvent(event)
+        }
+      },
+      error: (error: unknown) => {
+        if (error instanceof CorsOriginError) {
+          console.warn(
+            `Sanity Live is unable to connect to the Sanity API as the current origin - ${window.origin} - is not in the list of allowed CORS origins for this Sanity Project.`,
+            error.addOriginUrl && `Add it here:`,
+            error.addOriginUrl?.toString(),
+          )
+        } else {
+          console.error(error)
+        }
+      },
+    })
+    return () => subscription.unsubscribe()
+  }, [])
+
+  return null
+}
+SanityLive.displayName = 'SanityLive'
