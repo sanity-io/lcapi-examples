104f66ee48551ac9a76a902af517cf9efb85d5b9
Cody Olsen <stipsan@gmail.com>
2025-10-23 15:00:18 +0200

    remove refresh on focus

--- next-15/src/app/SanityLive.tsx
+++ next-15/src/app/SanityLive.tsx
@@ -51,30 +51,6 @@ export function SanityLive() {
     return () => subscription.unsubscribe()
   }, [])
 
-  return <RefreshOnFocus />
-}
-SanityLive.displayName = 'SanityLive'
-
-const focusThrottleInterval = 5_000
-function RefreshOnFocus() {
-  const router = useRouter()
-
-  useEffect(() => {
-    const controller = new AbortController()
-    let nextFocusRevalidatedAt = 0
-    const callback = () => {
-      const now = Date.now()
-      if (now > nextFocusRevalidatedAt && document.visibilityState !== 'hidden') {
-        router.refresh()
-        nextFocusRevalidatedAt = now + focusThrottleInterval
-      }
-    }
-    const {signal} = controller
-    document.addEventListener('visibilitychange', callback, {passive: true, signal})
-    window.addEventListener('focus', callback, {passive: true, signal})
-    return () => controller.abort()
-  }, [router])
-
   return null
 }
-RefreshOnFocus.displayName = 'RefreshOnFocus'
+SanityLive.displayName = 'SanityLive'
