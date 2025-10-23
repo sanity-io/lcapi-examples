73baabcdc38d38419b48f852def1d9f54cbd650b
Cody Olsen <81981+stipsan@users.noreply.github.com>
2025-10-23 18:07:55 +0200

    enable `cacheMode: 'noStale'` (#262)
    * enable `cacheMode: 'noStale'`
    
    * add inline docs

--- next-canary/src/sanity/fetch.ts
+++ next-canary/src/sanity/fetch.ts
@@ -3,9 +3,9 @@ import {cacheLife, cacheTag} from 'next/cache'
 import {client} from './client'
 
 /**
- * Next v6 has a first class API in `next-sanity` that should be used instead of this function.
+ * Next v16 has a first class API in `next-sanity` that should be used instead of this function.
  * It's here to show what the fetch function would look like if you were to implement it yourself, solving for production data.
- * The `defineLive` utility in `next-sanity` handles more advanced use cases, such as live preview, integrating with `sanity/presentation`, and more.
+ * The `defineLive` utility in `next-sanity@^12` handles more advanced use cases, such as live preview, integrating with `sanity/presentation`, and more.
  * @example
  * import {createClient, defineLive} from 'next-sanity/experimental/live'
  *  export const {sanityFetch, SanityLive} = defineLive({client: createClient({projectId, dataset, ...})})
@@ -21,9 +21,19 @@ export async function sanityFetch<const QueryString extends string>({
 }): Promise<{data: ClientReturn<QueryString, unknown>; tags?: string[]}> {
   'use cache: remote'
   const {result, syncTags} = await client.fetch(query, params, {
+    /**
+     * The default value is `filterResponse: true`, which is equivalent to `client.fetch(query, params, {filterResponse: false}).then(response => response.result)`,
+     * and we would not be able to access `response.syncTags`.
+     */
     filterResponse: false,
-    useCdn: false,
+    /**
+     * For optimal performance we want to use the API CDN and `perspective: 'published'`.
+     * The special `cacheMode: 'noStale'` flag instructs the Sanity API CDN to not serve stale content while the cache is background revalidating.
+     * Instead it'll wait for the background revalidation to complete, and then serve the updated content.
+     */
+    useCdn: true,
     perspective: 'published',
+    cacheMode: 'noStale',
   })
 
   const cacheTags = [...(syncTags || []), ...tags]
