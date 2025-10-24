4de32c89f230e9040249f9b876a855230719f086
Cody Olsen <stipsan@gmail.com>
2025-10-24 14:25:20 +0200

    fix font

--- next-16/src/app/dynamic/UserAgent.tsx
+++ next-16/src/app/dynamic/UserAgent.tsx
@@ -5,7 +5,7 @@ export async function UserAgent() {
   const userAgent = headersList.get('user-agent')
 
   return (
-    <div className="bg-(--theme-text) text-(--theme-background) absolute bottom-2 left-2 block rounded-sm  transition-colors duration-1000 ease-in-out">
+    <div className="bg-(--theme-text) text-(--theme-background) absolute bottom-2 left-2 block rounded-sm text-xs transition-colors duration-1000 ease-in-out">
       <span className="inline-block py-1 pl-2 pr-0.5">User Agent:</span>
       <span className="bg-(--theme-background) text-(--theme-text) mr-0.5 inline-block rounded-r-sm px-1 py-0.5 tabular-nums transition-colors duration-1000 ease-in-out">
         {userAgent}
