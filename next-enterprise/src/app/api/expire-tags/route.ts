1d0fedcf674f74c857e3534266470a7c0ab05099
Cody Olsen <stipsan@gmail.com>
2025-12-17 14:38:28 +0100

    ok it works

--- next-enterprise/src/app/api/expire-tags/route.ts
+++ next-enterprise/src/app/api/expire-tags/route.ts
@@ -10,9 +10,9 @@ export async function POST(request: Request) {
   const tags = url.searchParams.getAll('tag')
   console.log('Expiring tags from expirator service', tags)
   for (const tag of tags) {
-    // Temp test swr pattern
-    revalidateTag(tag, {expire: 0})
-    // revalidateTag(tag, {expire: 1})
+    // A 1s expiration (instead of 0) ensures the ISR cache is updated in the background, instead of blocking the `router.refresh()` render.
+    // This works since `router.refresh()` is called multiple times in a sequence to ensure distributed eventual consistency.
+    revalidateTag(tag, {expire: 1})
   }
   return Response.json(tags)
 }
